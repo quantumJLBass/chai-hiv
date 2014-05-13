3aec5740dc2493d27e5ef93df451c4d66c025aca
Jeremy Bass <jeremybass@cableone.net>
2014-05-13 12:30:09 -0700

    site_content is self generated so it doesn't need to exist

--- stellar/site_content/ams/cache/published/base/admin/content/js/tinymce/template_list.js
+++
@@ -1,5 +0,0 @@
-var tinyMCETemplateList = [
-// Name, URL, Description
-["Main Image left", "/Content/js/tinymce/templates/image.html", "Main Image"],
-["Layout", "/Content/js/tinymce/templates/layout1.html", "HTML Layout."]
-];
--- stellar/site_content/ams/themes/base/frontend/content/js/frontend/map_functions_create.js
+++
@@ -1,1 +0,0 @@
-var map_creation;
