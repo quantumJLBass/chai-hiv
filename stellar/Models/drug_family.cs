1eb00a6918f6857570a7bbf1010cf19e44951099
Jeremy Bass <jeremybass@cableone.net>
2014-06-22 16:04:07 -0700

    adds lmic models

--- stellar/Models/drug_family.cs
+++ stellar/Models/drug_family.cs
@@ -34,9 +34,11 @@ namespace stellar.Models {
         [HasAndBelongsToMany(typeof(drug), Lazy = true, Table = "drug_to_drug_family", ColumnKey = "drug_family_id", ColumnRef = "drug_id", NotFoundBehaviour = NotFoundBehaviour.Ignore)]
         virtual public IList<drug> drugs { get; set; }
 
-        [HasAndBelongsToMany(typeof(drug), Lazy = true, Table = "drug_to_drug_interaction", ColumnKey = "drug_family_id", ColumnRef = "interaction_id", NotFoundBehaviour = NotFoundBehaviour.Ignore)]
-        virtual public IList<drug> drugs { get; set; }
+        [HasAndBelongsToMany(typeof(drug_interaction), Lazy = true, Table = "drug_to_drug_interaction", ColumnKey = "drug_family_id", ColumnRef = "interaction_id", NotFoundBehaviour = NotFoundBehaviour.Ignore)]
+        virtual public IList<drug_interaction> interactions { get; set; }
 
+        [HasAndBelongsToMany(typeof(drug_lmic), Lazy = true, Table = "drug_family_to_drug_lmic", ColumnKey = "drug_family_id", ColumnRef = "lmic_id", NotFoundBehaviour = NotFoundBehaviour.Ignore)]
+        virtual public IList<drug_lmic> lmics { get; set; }
     }
 
     [ActiveRecord(Lazy = true, BatchSize = 5)]
@@ -50,6 +52,15 @@ namespace stellar.Models {
 
     }
 
+    [ActiveRecord(Lazy = true, BatchSize = 5)]
+    public class drug_lmic : publish_base {
+        [JoinedKey("lmic_id")]
+        virtual public int id { get; set; }
 
+
+        [HasAndBelongsToMany(typeof(drug), Lazy = true, Table = "drug_family_to_drug_lmic", ColumnKey = "lmic_id", ColumnRef = "drug_family_id", NotFoundBehaviour = NotFoundBehaviour.Ignore)]
+        virtual public IList<drug> drugs { get; set; }
+
+    }
 
 }
