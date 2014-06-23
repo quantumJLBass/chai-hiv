ac32c960b8c61b17ce1dbed9bb6cb0310234af74
Jeremy Bass <jeremybass@cableone.net>
2014-06-22 22:59:21 -0700

    adds grunt for the js

---
+++ stellar/Content/Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		env : {
+			options : {
+				/* Shared Options Hash */
+				//globalOption : 'foo'
+			},
+			dev: {
+				NODE_ENV : 'DEVELOPMENT'
+			},
+			prod : {
+				NODE_ENV : 'PRODUCTION'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+					'/*  See https://github.com/washingtonstateuniversity/WSU-spine/ for full source.*/\n'
+			},
+			build: {
+				src: 'src/js/init.js',
+				dest: 'js/init.js'
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					// Hmmm, in reverse order
+					'css/style.css': ['src/css/style.css']
+				}
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/js/*.js'],
+			options: {
+				// options here to override JSHint defaults
+				boss: true,
+				curly: true,
+				eqeqeq: true,
+				eqnull: true,
+				expr: true,
+				immed: true,
+				noarg: true,
+				onevar: true,
+				quotmark: "double",
+				smarttabs: true,
+				trailing: true,
+				undef: true,
+				unused: true,
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true,
+					window:true
+				}
+			}
+		},
+		
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-env');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('prod', ['env:prod', 'concat','preprocess:js','cssmin','uglify','copy','includereplace','preprocess:html']);	
+	
+	grunt.registerTask('dev', ['jshint', 'env:dev', 'cssmin', 'uglify', ]);
+		
+		
+};
